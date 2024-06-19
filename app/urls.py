84f9a5a745eabb888e5b7f71279a7f88b83e62a9
Bemontx <101949185+Bemontx@users.noreply.github.com>
2024-06-19 01:01:05 -0500

    form/register

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import IndexView, HomeView
 
 urlpatterns = [
-    path('', HomeView.as_view({'get': 'home'}), name='home'),
+    path('', HomeView.as_view({'get': 'home', 'post':'register'}), name='home'),
     path('index/', IndexView.as_view({'get': 'list'}), name='index'),
 ]
--- app/views.py
+++ app/views.py
@@ -6,6 +6,24 @@ from .serializers import UsersSerializer, PetsSerializer
 from .models import Users, Pets
 
 # Create your views here.
+##clase inicial pag HOME
+class HomeView(viewsets.ViewSet):
+    
+    def home(self,request):
+        return render(request,'home.html')
+    
+    def register(self,request):
+        if request.method == 'POST':
+            serializer = UsersSerializer(data=request.POST)
+            if serializer.is_valid():
+                serializer.save()
+                return('index')
+        else:
+            serializer = UsersSerializer()
+            
+        return render(request, 'register.html',{'serializer': serializer})
+
+##clase template index
 class IndexView(viewsets.ViewSet):
     permission_classes = [AllowAny]
     authentication_classes = [authentication.SessionAuthentication]
@@ -17,7 +35,5 @@ class IndexView(viewsets.ViewSet):
             'users': serializer.data
         }
         return render(request, 'index.html', context)
-class HomeView(viewsets.ViewSet):
     
-    def home(self,request):
-        return render(request,'home.html')
+    
